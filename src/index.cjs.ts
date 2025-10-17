efbc2efe3dfe1e946fb8103238f092b903ddfe21
Obed Diaz <obed.diaz@scouting.org>
2025-10-17 13:32:55 -0600

    FIX: fix issue when using require instead of import

---
+++ src/index.cjs.ts
@@ -0,0 +1,5 @@
+import createRoutePaths, { generatePath } from "./index";
+
+module.exports = createRoutePaths;
+module.exports.default = createRoutePaths;
+module.exports.generatePath = generatePath;
